f65f32306d881642b1af125d791556ff99b66636
BirdPersonBartz <griffbartzen@gmail.com>
2016-03-08 22:58:10 -0500

    got some of my stats to load
    getting a weird invalid syntax error

--- stats.py
+++ stats.py
@@ -19,12 +19,23 @@ data = '''Region,Alcohol,Tobacco
 data = data.splitlines()
 data = [i.split(',') for i in data]
 
+
+#assigns column_names to the row with the headers
 column_names = data[0]
+
+#assigns data_rows to the remeaing rows of data
 data_rows = data[1::]
+
+#builds the PD data frame from the assignments above
 df = pd.DataFrame(data_rows, columns=column_names)
 
-
+#float the numbers
 df['Alcohol'] = df['Alcohol'].astype(float)
 df['Tobacco'] = df['Tobacco'].astype(float)
 
-print("The mean for the Alcohol and Tobacco dataset is %s for Alcohol and %s for Tobacco" % round((df['Alcohol'].mean(),3), round(df['Tobacco'].mean(),3))
+print("The mean for the Alcohol and Tobacco dataset is %s for Alcohol and %s for Tobacco" % (round(df['Alcohol'].mean(),3), round(df['Tobacco'].mean(),3)))
+print("The median for the Alcohol and Tobacco dataset is %s for Alcohol and %s for Tobacco" % (df['Alcohol'].median(), df['Tobacco'].median()))
+print("The mode for the Alcohol and Tobacco dataset is %s for Alcohol and %s for Tobacco" % (stats.mode(df['Alcohol']), stats.mode(df['Tobacco']))
+print("The range for the Alcohol and Tobacco dataset is %s for Alcohol and %s for Tobacco" % (max(df['Alcohol'])-min(df['Alcohol']), max(df['Tobacco'])-min(df['Tobacco']))
+print("The variance for the Alcohol and Tobacco dataset is %s for Alcohol and %s for Tobacco" % (round(df['Alcohol'].var(),3), round(df['Tobacco'].var(),3)))
+print("The standard deviation for the Alcohol and Tobacco dataset is %s for Alcohol and %s for Tobacco" % (round(df['Alcohol'].std(),3), round(df['Tobacco'].std(),3)))
