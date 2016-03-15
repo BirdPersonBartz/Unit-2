294d303721f460f236a06a3a8399bfa466998b98
BirdPersonBartz <griffbartzen@gmail.com>
2016-03-14 22:42:35 -0400

    updated chi_squared.py

--- chi_squared.py
+++ chi_squared.py
@@ -1,5 +1,4 @@
 from scipy import stats
-
 import matplotlib.pyplot as plt
 import collections
 import pandas as pd
@@ -7,12 +6,14 @@ import pandas as pd
 loansData = pd.read_csv('https://github.com/Thinkful-Ed/curric-data-001-data-sets/raw/master/loans/loansData.csv')
 loansData.dropna(inplace=True)
 
-plt.figure()
-chip= stats.chisquare(loansData['Open.CREDIT.Lines'])
+freq = collections.Counter(loansData['Open.CREDIT.Lines'])
 
 
 
-plt.show()
+chi, p = stats.chisquare(freq.values())
+
+print(chi, p)
+
 
 
 
